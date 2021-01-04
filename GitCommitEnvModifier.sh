46e29130beeff94b8385da6af4c78ae25bea50d4
madeuth <19433927+madeuth@users.noreply.github.com>
2021-01-05 00:31:15 +0100

    script v1

---
+++ GitCommitEnvModifier.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# Globals
+NAME="name"
+EMAIL="email"
+DOMAIN="github.com"
+
+# Greetings
+echo "Welcome to the git commit environment variables modifier assistant."
+echo "You can press 'ctrl+c' anytime to quit."
+
+while true
+do
+    # Get repository name
+    echo -n "Type in a repo's name to clone: "
+    read repo
+    git clone https://$NAME@$DOMAIN/$NAME/$repo.git
+
+    # Moving inside created directory
+    cd $repo
+
+    # Get username to filter out commits
+    echo -n "Type in a username to match (leave blank to match all commits): "
+    read match
+
+    # If match provided then filter out commits
+    if [ -n "$match" ] 
+    then
+        git filter-branch -f --env-filter '
+        if [ "$GIT_AUTHOR_NAME" = '$match' ]
+        then
+            export GIT_AUTHOR_NAME='$NAME'
+            export GIT_AUTHOR_EMAIL='$EMAIL'
+        fi
+        if [ "$GIT_COMMITTER_NAME" = '$match' ]
+        then
+            export GIT_COMMITTER_DATE="$GIT_COMMITTER_DATE"
+            export GIT_COMMITTER_NAME='$NAME'
+            export GIT_COMMITTER_EMAIL='$EMAIL'
+        fi
+        '
+    # else update all commits
+    else
+        git filter-branch -f --env-filter '
+        export GIT_COMMITTER_DATE="$GIT_COMMITTER_DATE"
+        export GIT_AUTHOR_NAME='$NAME'
+        export GIT_AUTHOR_EMAIL='$EMAIL'
+        export GIT_COMMITTER_NAME='$NAME'
+        export GIT_COMMITTER_EMAIL='$EMAIL'
+        '
+    fi
+
+    # Push modifications to server
+    git push -f https://$NAME@$DOMAIN/$NAME/$repo.git
+    
+    # Moving out of directory
+    cd ..
+
+    # Removing directory
+    rm -r -f $repo
+
+done
+
+exit
